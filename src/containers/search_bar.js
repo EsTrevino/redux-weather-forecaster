4c8ba990213ee08c8e07c13773f43de5861ff637
EsTrevino <sttrevino2@gmail.com>
2018-04-17 13:25:19 -0700

    first commit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component{
+  constructor(props){
+    super(props);
+
+    this.state={
+      term: ''
+    }
+    this.onInputChange=this.onInputChange.bind(this);
+    this.onFormSubmit=this.onFormSubmit.bind(this);
+  }
+  onInputChange(event){
+    this.setState({term: event.target.value})
+  }
+  onFormSubmit(event){
+    event.preventDefault();
+    console.log(this.state.term);
+  }
+  render(){
+    return(
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          className="form-control"
+          placeholder="Get a five-day forecast in your favorite cities"
+          value={this.state.term}
+          onChange={this.onInputChange} />
+        <span className="input-group-button">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    )
+  }
+}
