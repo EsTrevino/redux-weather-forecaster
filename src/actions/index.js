4fcbdf111ac1758f4b2c06e48a50314095c339fa
EsTrevino <sttrevino2@gmail.com>
2018-04-19 14:42:46 -0700

    added redux api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "26839131696992a3553a44f643a4f407";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
